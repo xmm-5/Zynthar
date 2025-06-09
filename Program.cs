fb02c5499d6a44b7c11b729abea23d48829e1f76
ZOOMEXX <zoomexxdev@gmail.com>
2025-06-09 17:17:19 +0200

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,38 @@
+﻿using DSharpPlus;
+using DSharpPlus.CommandsNext;
+using System.Threading.Tasks;
+using Zynthar.config;
+
+namespace Zynthar
+{
+    class Program
+    {
+        private static DiscordClient Client { get; set; }
+        private static CommandsNextExtension Commands { get; set; }
+        static async Task Main(string[] args)
+        {
+            var jsonReader = new JSONReader();
+            await jsonReader.ReadJSON();
+
+            var discordConfig = new DiscordConfiguration()
+            {
+                Intents = DiscordIntents.All,
+                Token = jsonReader.token,
+                TokenType = TokenType.Bot,
+                AutoReconnect = true
+            };
+
+            Client = new DiscordClient(discordConfig);
+
+            Client.Ready += Client_Ready;
+
+            await Client.ConnectAsync();
+            await Task.Delay(-1);
+        }
+
+        private static Task Client_Ready(DiscordClient sender, DSharpPlus.EventArgs.ReadyEventArgs args)
+        {
+            return Task.CompletedTask;
+        }
+    }
+}
---
+++ config/JSONReader.cs
@@ -0,0 +1,31 @@
+﻿using DSharpPlus.Net.Serialization;
+using Newtonsoft.Json;
+using System.IO;
+using System.Threading.Tasks;
+
+namespace Zynthar.config
+{
+    public class JSONReader
+    {
+        public string token { get; set; }
+        public string prefix { get; set; }
+
+        public async Task ReadJSON()
+        {
+            using (StreamReader sr = new StreamReader("config.json"))
+            {
+                string json = sr.ReadToEnd();
+                JSONStructure data = JsonConvert.DeserializeObject<JSONStructure>(json);
+
+                this.token = data.token;
+                this.prefix = data.prefix;
+            }
+        }
+    }
+
+    internal sealed class JSONStructure
+    {
+        public string token { get; set; }
+        public string prefix { get; set; }
+    }
+}
